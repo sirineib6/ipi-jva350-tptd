50a8697e78a367bf134272df32ef14333056c545
sirineib6 <ibrsirine@gmail.com>
2025-03-19 23:30:53 +0100

    test SalarieAideADomicileServiceMoce

--- src/test/java/com/ipi/jva350/service/SalarieAideADomicileServiceMock.java
+++ src/test/java/com/ipi/jva350/service/SalarieAideADomicileServiceMock.java
@@ -15,28 +15,34 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.time.LocalDate;
 
 @ExtendWith(MockitoExtension.class)
-public class SalarieAideADomicileServiceMock{
+public class SalarieAideADomicileServiceMock {
 
     @Mock
     private SalarieAideADomicileRepository salarieAideADomicileRepository;
-    @InjectMocks
-    private SalarieAideADomicileService salarieService = new SalarieAideADomicileService();
 
-    // mocké :
+    @InjectMocks
+    private SalarieAideADomicileService salarieService;
+
+    /**
+     * Test unitaire de la méthode ajouteConge() (mocké).
+     * Ce test vérifie que les congés payés sont correctement ajoutés et que le repository est appelé pour la sauvegarde.
+     */
     @Test
     public void testAjouteConge() throws SalarieException {
-        // Given :
+        // Given : création d'un salarié avec des informations spécifiques
         SalarieAideADomicile monSalarie = new SalarieAideADomicile("Paul",
                 LocalDate.of(2022, 6, 28), LocalDate.of(2023, 11, 1),
                 9, 2.5,
                 80, 20, 8);
-        // When :
-        salarieService.ajouteConge(monSalarie, LocalDate.of(2024, 12, 17),
-                LocalDate.of(2024, 12, 18));
-        // Then :
-        ArgumentCaptor<SalarieAideADomicile> salarieAideADomicileCaptor = ArgumentCaptor.forClass(SalarieAideADomicile.class);
-        Mockito.verify(salarieAideADomicileRepository, Mockito.times(1)).save(salarieAideADomicileCaptor.capture()); // arg capture !
-        Assertions.assertEquals(1L, salarieAideADomicileCaptor.getValue().getCongesPayesPrisAnneeNMoins1());
-    }
 
+        // When : appel de la méthode pour ajouter un congé
+        salarieService.ajouteConge(monSalarie, LocalDate.of(2024, 12, 17), LocalDate.of(2024, 12, 18));
+
+        // Then : vérifier que la méthode save() a bien été appelée sur le repository
+        ArgumentCaptor<SalarieAideADomicile> salarieCaptor = ArgumentCaptor.forClass(SalarieAideADomicile.class);
+        Mockito.verify(salarieAideADomicileRepository, Mockito.times(1)).save(salarieCaptor.capture());
+
+        // Vérifier que le nombre de congés pris en année N-1 a bien été mis à jour après l'ajout du congé
+        Assertions.assertEquals(1L, salarieCaptor.getValue().getCongesPayesPrisAnneeNMoins1());
+    }
 }
